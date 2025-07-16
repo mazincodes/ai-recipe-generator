e3fe7fbce1930fe6776b5f03f7397ca9f3760799
mazincodes <mazinshakeel72@gmail.com>
2025-07-16 01:55:12 -0800

    Updated model

--- src/ai.js
+++ src/ai.js
@@ -12,7 +12,8 @@ export async function getRecipeFromMistral(ingredientsArr) {
     const ingredientsString = ingredientsArr.join(", ")
     try {
         const response = await hf.chatCompletion({
-            model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
+            // model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
+            model: "moonshotai/Kimi-K2-Instruct",
             messages: [
                 { role: "system", content: SYSTEM_PROMPT },
                 { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!` },
