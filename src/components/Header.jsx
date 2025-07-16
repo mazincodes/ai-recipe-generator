86859b1e3344dccc7a07c84c31465d622ee2eca6
mazincodes <mazinshakeel72@gmail.com>
2025-07-16 01:45:22 -0800

    Updated feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import chefSeekLogo from '../images/chefSeekLogo.png'
 function Header() {
   return (
-      <header className='flex justify-center items-center md:w-full gap-4 bg-[#7ac9fa] p-2'>
+      <header className='flex justify-center items-center md:w-full gap-4 bg-[#979932] p-2'>
         <img className="w-[150px] shadow-[0_3px_8px_rgba(0,0,0,0.3)] bg-linear-to-t from-[#738c69] to-[#88a17e] rounded-full p-2" src={chefSeekLogo} />
         <h1 className='text-4xl text-[#333] font-[poppins] p-4'>Chef Seek</h1>
       </header>
